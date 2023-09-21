cb5f86695f5a247e4325588662bf37bdfd17df1f
winh321 <69839416+winh321@users.noreply.github.com>
2023-09-21 17:17:45 +0630

    Create card-data.js

---
+++ React Card/data/card-data.js
@@ -0,0 +1,27 @@
+export const cardarray =[
+    {
+        id:1,
+    image:"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg",
+    title:"First Card",
+    description:"Card description 1"},
+    {
+        id:2,
+    image:"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg",
+    title:"Second Card",
+    description:"Card description 2"},
+    {
+        id:3,
+    image:"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg",
+    title:"Third Card",
+    description:"Card description 3"},
+    {
+        id:4,
+        image:"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg",
+        title:"Fourth Card",
+        description:"Card description 4"},
+        {
+            id:5,
+        image:"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg",
+        title:"Fifth Card",
+        description:"Card description 5"}
+    ]
